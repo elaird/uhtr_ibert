a348ea54e9b0cecbd06ac29aa8ff74dfd99039ce
Edward Laird <elaird@gmail.com>
2014-09-17 14:03:50 +0200

    support ctp7

--- common.py
+++ common.py
@@ -12,12 +12,22 @@ def versions(sn=None):
     return ""
 
 
-def results(sn, gtx, run, okLen=11):
+def results(sn, gtx, run, ctp7=False):
     d = {-5: " RX Sampling Point(tap)",
           -2: " # Errs",
           -1: " BER",
           }
-    fileName = "optical_loopback/uHTR%03d/%s_sweep_results%s.csv" % (sn, gtx, run)
+    if ctp7:
+        del d[-2]
+        d[-3] = " # Errs1"
+        okLen = 10
+    else:
+        okLen = 11
+
+    if ctp7:
+        fileName = "ctp7/%03d/%s_sweep_results%s.csv" % (sn, gtx, run)
+    else:
+        fileName = "optical_loopback/uHTR%03d/%s_sweep_results%s.csv" % (sn, gtx, run)
 
     out = []
     try:
@@ -25,6 +35,7 @@ def results(sn, gtx, run, okLen=11):
     except IOError, e:
         print e
         return out
+
     headers = []
     for row in csv.reader(f):
         if len(row) != okLen:
@@ -34,7 +45,7 @@ def results(sn, gtx, run, okLen=11):
         else:
             try:
                 phase = int(row[-5])
-                nErrors = None #int(row[-3])
+                nErrors = int(row[-3]) if ctp7 else None
                 ber = float(row[-1])
                 out.append( (phase, nErrors, ber))
             except ValueError:
@@ -46,24 +57,26 @@ def results(sn, gtx, run, okLen=11):
     return out
 
 
-def graphs(xMin=0.0, xMax=1.0, uhtrs=[], runs=[""]):
+def graphs(xMin=0.0, xMax=1.0, uhtrs=[], runs=[""], ctp7=False):
+    gtxes = range(113, 116) if ctp7 else range(112, 118)
+
     out = {}
     for uhtr in uhtrs:
-        for gtx100 in range(112, 118):
+        for gtx100 in gtxes:
             for gtx4 in range(4):
                 for run in runs:
                     key = (uhtr, gtx100, gtx4, run)
 
                     g = r.TGraph()
-                    gtx = "GTX%d_%d" % (gtx4, gtx100)
+                    gtx = "%s%d_%d" % ("GTH" if ctp7 else "GTX", gtx4, gtx100)
                     g.SetName("%d_%s_%s" % (uhtr, gtx, run))
-                    res = results(uhtr, gtx, run)
+                    res = results(uhtr, gtx, run, ctp7=ctp7)
                     if not res:
                         continue
                     phases = [x[0] for x in res]
-                    pMin = 0 # min(phases)
-                    pMax = 127 # max(phases)
-                    
+                    pMin = min(phases) if xMin else 0
+                    pMax = max(phases) if xMin else 127
+
                     assert (pMax - pMin), "%d_%d" % (pMax, pMin)
                     for i, (phase, nErrors, ber) in enumerate(res):
                         x = xMin + (xMax - xMin) * (phase - pMin) / (pMax - pMin)
