43e2dd1824bd7a0553bf032194172c39fb972feb
Edward Laird <elaird@gmail.com>
2014-09-18 09:50:09 +0200

    include ctp7 plot

--- plot.py
+++ plot.py
@@ -6,13 +6,27 @@ import ROOT as r
 
 
 def summary(fileName="", uhtrs=[], runs=[""], pad=lambda x: 1,
-            berMin=1.863e-13, berMax=1.0e-2, samplePoint=0.598,
-            begin=False, end=False):
+            berMin=1.863e-13, berMax=1.0e-2,
+            ctp7=False, begin=False, end=False):
+
     assert fileName
     assert berMin
     assert runs
 
-    grs = common.graphs(uhtrs=uhtrs, runs=runs)
+    if ctp7:
+        xMin = -0.5
+        xMax = 0.5
+        samplePoint = None
+    else:
+        xMin = 0.0
+        xMax = 1.0
+        samplePoint = 0.598
+
+    grs = common.graphs(uhtrs=uhtrs,
+                        runs=runs,
+                        xMin=xMin,
+                        xMax=xMax,
+                        ctp7=ctp7)
     c = r.TCanvas()
     c.Divide(2, 2)
 
@@ -20,7 +34,7 @@ def summary(fileName="", uhtrs=[], runs=[""], pad=lambda x: 1,
         c.cd(0)
         c.Print(fileName+"[")
 
-    h = r.TH2D("h", ";Horizontal Offset (UI); BER", 1, 0.0, 1.0, 1, berMin, berMax)
+    h = r.TH2D("h", ";Horizontal Offset (UI); BER", 1, xMin, xMax, 1, berMin, berMax)
     h.SetStats(False)
 
     keep = []
@@ -38,22 +52,38 @@ def summary(fileName="", uhtrs=[], runs=[""], pad=lambda x: 1,
         c.cd(iPad)
         if not drawn[iPad]:
             h2 = h.DrawClone()
-            h2.SetTitle("%s, optical loop-back, 6.4 Gbps" % common.versions(uhtr))
+            if ctp7:
+                h2.SetTitle("uHTR s/n %d" % uhtr)
+            else:
+                h2.SetTitle("%s, optical loop-back, 6.4 Gbps" % common.versions(uhtr))
             keep.append(h2)
+
             drawn[iPad] = True
-            leg = r.TLegend(0.14, 0.85, 0.44, 0.15)
+            if ctp7:
+                leg = r.TLegend(0.44, 0.85, 0.64, 0.15)
+            else:
+                leg = r.TLegend(0.14, 0.85, 0.44, 0.15)
             leg.SetBorderSize(0)
             leg.SetFillStyle(0)
-            leg.SetHeader("uHTR s/n %d" % uhtr)
+            if ctp7:
+                leg.SetHeader("CTP7 GTH")
+            else:
+                leg.SetHeader("uHTR s/n %d" % uhtr)
             keep.append(leg)
             if samplePoint is not None:
                 line.DrawLine(samplePoint, berMin, samplePoint, berMax)
                 leg.AddEntry(line, "IBERT sampling point", "l")
             leg.Draw("same")
 
+        if ctp7:
+            iColor = 3 * gtx4 + gtx100 - 113
+            colors = range(1, 10) + [11, 25, 28]
+            colors[4] = r.kOrange - 3
+            color = colors[iColor]
+        else:
+            iColor = 1 + gtx4 + 4*(gtx100 - 112)
+            color = r.TColor.GetColorPalette(2 * iColor)
 
-        iColor = 1 + gtx4 + 4*(gtx100 - 112)
-        color = r.TColor.GetColorPalette(2 * iColor)
         g.SetLineStyle(1 + ((iColor-1) % 3))
         g.SetLineColor(color)
         g.SetMarkerColor(color)
@@ -68,7 +98,10 @@ def summary(fileName="", uhtrs=[], runs=[""], pad=lambda x: 1,
             name += " (100s)"
         else:
             name += " (1s)"
-        leg.AddEntry(g, name, "l")
+        if ctp7:
+            leg.AddEntry(g, "%d_%d" % (gtx100, gtx4), "lp")
+        else:
+            leg.AddEntry(g, name, "l")
 
     c.cd(0)
     c.Print(fileName)
@@ -77,6 +110,14 @@ def summary(fileName="", uhtrs=[], runs=[""], pad=lambda x: 1,
         c.cd(0)
         c.Print(fileName+"]")
 
+
+def ctp7():
+    fileName = "uhtr_ctp7.pdf"
+    summary(fileName=fileName,
+            berMin=1.863e-10,
+            uhtrs=[103, 104, 105, 106],
+            pad=lambda uhtr: uhtr - 102,
+            ctp7=True)
 
 def reworked():
     fileName = "reworked_uhtrs.pdf"
@@ -101,3 +142,4 @@ if __name__ == "__main__":
 
     reworked()
     new()
+    ctp7()
